e955364dc932578a0bfbe9307f483626ac8a32a0
Инара <inarashugaikina@gmail.com>
2022-01-28 13:32:16 +0600

    Create CustomUser model with required fields

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    class Gender(models.TextChoices):
+        FEMALE = 'female'
+        MALE = 'male'
+
+    avatar = models.ImageField(upload_to='media/', blank=True, null=True)
+    gender = models.CharField(choices=Gender.choices, max_length=200)
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name', 'avatar', 'gender']
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def __str__(self):
+        return self.first_name + self.last_name
