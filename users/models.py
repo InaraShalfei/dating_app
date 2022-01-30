259873207e85c40a6781d99197f034115748818d
InaraShalfei <inarashugaikina@gmail.com>
2022-01-30 11:11:48 +0500

    Add ordering for CustomUser model and validators for longitude and latitude values

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 from users.managers import CustomUserManager
@@ -14,13 +15,18 @@ class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=150, blank=False)
     last_name = models.CharField(max_length=150, blank=False)
     email = models.EmailField(blank=False, unique=True)
-    longitude = models.FloatField(blank=False)
-    latitude = models.FloatField(blank=False)
+    longitude = models.FloatField(blank=False, validators=[
+        MinValueValidator(-180.0), MaxValueValidator(180.0)
+    ])
+    latitude = models.FloatField(blank=False, validators=[
+        MinValueValidator(-90.0), MaxValueValidator(90.0)
+    ])
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name', 'avatar', 'gender',
                        'longitude', 'latitude']
 
     class Meta:
+        ordering = ('first_name', 'last_name',)
         verbose_name = 'user'
         verbose_name_plural = 'users'
 
@@ -31,8 +37,10 @@ class CustomUser(AbstractUser):
 
 
 class UserFollow(models.Model):
-    follower = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_following')
-    followed = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_followed')
+    follower = models.ForeignKey(CustomUser, on_delete=models.CASCADE,
+                                 related_name='user_following')
+    followed = models.ForeignKey(CustomUser, on_delete=models.CASCADE,
+                                 related_name='user_followed')
 
     class Meta:
         verbose_name = 'Following user'
